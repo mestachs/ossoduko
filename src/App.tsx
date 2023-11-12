38c033fc80f53c901b5b949b0b1a26ea02e79b5e
Stéphan Mestach <smestach@bluesquare.org>
2023-11-12 14:07:52 +0100

    Step by step solution

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,39 @@ interface Coordinates {
   j: number;
 }
 
-const removePossibilitiesForCells = function (cells: Cell[]) {
+const removePossibilitiesForCells = function (
+  cells: Cell[],
+  stopAtFirst = true
+): Cell | null {
   const resolvedPossibilities = cells
     .filter((c) => c.isSolved())
     .map((c) => c.possibilities[0]);
   for (let cell of cells.filter((c) => !c.isSolved())) {
     cell.removePossibilities(resolvedPossibilities);
+    if (cell.possibilities.length == 1 && stopAtFirst) {
+      cell.markAsSolved();
+      return cell;
+    }
   }
+  return null;
 };
 
 class Cell {
   public possibilities: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   public block?: Coordinates;
   public coordinates: Coordinates;
+  public frozen: boolean = false;
+  public solved: boolean = false;
 
   constructor(i: number, j: number) {
     this.coordinates = { i, j };
   }
   public isSolved(): boolean {
-    return this.possibilities.length == 1;
+    return this.solved;
+  }
+
+  public markAsSolved() {
+    this.solved = true;
   }
 
   public assignBlock(i: number, j: number) {
@@ -37,6 +51,11 @@ class Cell {
     this.possibilities = this.possibilities.filter(
       (p) => !resolvedPossibilities.includes(p)
     );
+  }
+
+  public freeze() {
+    this.frozen = true;
+    this.markAsSolved();
   }
 
   public toString(): string {
@@ -59,10 +78,19 @@ class Game {
     this.getBlocks();
   }
 
-  public play(rowNumber: number, colNumber: number, possibility: number) {
+  public play(
+    rowNumber: number,
+    colNumber: number,
+    possibility: number,
+    freeze = false
+  ) {
     if (this.cells) {
       const row = this.cells[rowNumber];
       row[colNumber].possibilities = [possibility];
+      row[colNumber].markAsSolved();
+      if (freeze) {
+        row[colNumber].freeze();
+      }
     }
   }
 
@@ -113,16 +141,25 @@ class Game {
     throw new Error("no cell");
   }
 
-  public adaptPossibilities() {
+  public adaptPossibilities(stopAtFirst = true) {
     if (this.cells) {
       for (let row of this.getRows()) {
-        removePossibilitiesForCells(row);
+        const cell = removePossibilitiesForCells(row, stopAtFirst);
+        if (cell && stopAtFirst) {
+          return cell;
+        }
       }
       for (let column of this.getColumns()) {
-        removePossibilitiesForCells(column);
+        const cell = removePossibilitiesForCells(column, stopAtFirst);
+        if (cell && stopAtFirst) {
+          return cell;
+        }
       }
       for (let block of this.getBlocks()) {
-        removePossibilitiesForCells(block);
+        const cell = removePossibilitiesForCells(block, stopAtFirst);
+        if (cell && stopAtFirst) {
+          return cell;
+        }
       }
     }
   }
@@ -143,7 +180,7 @@ class GameFactory {
           " => ",
           rowNumber + " " + colNumber + "  : " + possibility + " " + c
         );
-        game.play(rowNumber, colNumber, possibility);
+        game.play(rowNumber, colNumber, possibility, true);
       }
       index = index + 1;
     }
@@ -153,11 +190,16 @@ class GameFactory {
 
 function App() {
   const puzzlesAsString = [
-    "..17..5.9573.241.68..5.1..27..295.18..94..3.56528....7465.8..71...159..49.8..7.53",
+    "..17..5.9573.241.68..5.1..27..295.18..94..3.56528....7465.8..71...159..49.8..7.5.",
     "524..6.........7.13...........4..8..6......5...........418.........3..2...87.....",
+    "1....7.9..3..2...8..96..5....53..9...1..8...26....4...3......1..4......7..7...3..",
+    "..............3.85..1.2.......5.7.....4...1...9.......5......73..2.1........4...9",
+    "4.....8.5.3..........7......2.....6.....8.4......1.......6.3.7.5..2.....1.4......",
+    "82.6..9.5.............2.31...7318.6.24.....73...........279.1..5...8..36..3......",
   ];
   const [game, _] = useState(GameFactory.fromStringPuzzle(puzzlesAsString[0]));
   const [count, setCount] = useState(0);
+  const [justSolvedCell, setJustSolvedCell] = useState<Cell | null>(null);
 
   function pickPossibleChoices(cell: Cell) {
     let rawValue = prompt("Which value ? " + cell.toString());
@@ -179,14 +221,16 @@ function App() {
               <td
                 width="120"
                 height="120"
-                title={cell.block?.i + " " + cell.block?.j}
+                title={cell.possibilities.join(" ")}
                 className={[
                   "block-" + cell.block?.i + "-" + cell.block?.j,
                   cell.isSolved() ? "solved" : "unsolved",
+                  justSolvedCell === cell ? "justSolved" : "",
+                  cell.frozen ? "frozen" : "",
                 ].join(" ")}
                 onClick={() => pickPossibleChoices(cell)}
               >
-                {cell.toString()}
+                {cell.isSolved() ? cell.toString() : " "}
               </td>
             ))}
           </tr>
@@ -195,7 +239,8 @@ function App() {
       <div>
         <button
           onClick={() => {
-            game.adaptPossibilities();
+            const cell = game.adaptPossibilities();
+            setJustSolvedCell(cell);
             setCount(count + 1);
           }}
         >
